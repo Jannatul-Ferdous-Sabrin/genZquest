23df24e412dc88872cb47e300ce441b25f1c7a57
Sabrin <cse_2012020251@lus.ac.bd>
2023-08-04 21:12:16 +0600

    contact us page added

---
+++ contact.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css">
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
+/>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-pzjw5f+uaOVpKGBJgE18a/dz9UqMsu6Jr9GgokpJ7lIbbVYUew+OrCXaRkfjWPM" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="style.css" >
+<style>
+ *{
+    font-family: 'Poppins', sans-serif; 
+ }
+
+ .h-font{
+    font-family: 'Merienda', cursive;
+ }
+
+  
+</style>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-lg-2 shadow-sm sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">genZquest</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active me-2" aria-current="page" href="index.php">Home</a>
+        </li>
+
+         <li class="nav-item">
+        <a class="nav-link me-2" href="#about">About</a>
+      </li>
+       <li class="nav-item">
+          <a class="nav-link me-2" href="contact.php">Contact Us</a>
+        </li>
+
+
+        <li class="nav-item">
+          <a class="nav-link" href="./signup.php" > <i class="bi bi-person"></i>Register</a>
+        </li>
+
+         <li class="nav-item">
+    <a class="nav-link" href="./login.php"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+  </li>
+  </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<!-- Container (Contact Section) -->
+<div id="contact" class="container-fluid bg-grey">
+    <h2 class="text-center py-5 m-auto" style="color:rgb(7,7, 78);">CONTACT</h2>
+    <div class="row">
+      <div class="col-sm-5">
+        <p>Contact us and we'll get back to you within 24 hours.</p>
+        <p><span class="bi bi-geo-alt-fill"></span> Sylhet, Bangladesh</p>
+
+        <p><span class="bi bi-telephone-fill"></span> +8801777777777</p>
+
+        
+        <p><span class="bi bi-envelope"></span><a href="mailto:sabb@gmail.com"> sabb@gmail.com</a></p>
+      </div>
+
+   <div class="col-sm-7 slideanim">
+      <form>
+  <!-- Name input -->
+  <div class="form-outline mb-4">
+  <label class="form-label" for="form4Example1">Name</label>
+    <input type="text" id="form4Example1" class="form-control" />
+   
+  </div>
+
+  <!-- Email input -->
+  <div class="form-outline mb-4">
+  <label class="form-label" for="form4Example2">Email address</label>
+    <input type="email" id="form4Example2" class="form-control" />
+    
+  </div>
+
+  <!-- Message input -->
+  <div class="form-outline mb-4">
+  <label class="form-label" for="form4Example3">Message</label>
+  <textarea class="form-control" id="form4Example3" rows="4"></textarea>
+    
+  </div>
+
+  <!-- Submit button -->
+  <button type="submit" class="btn btn-primary btn-block mb-4">Send </button>
+
+        <div class="text-center py-3">
+                <p>or sign up with:</p>
+                <button type="button" class="btn btn-outline-primary btn-floating mx-1">
+                    <i class="fab fa-facebook-f" style="color: #1877F2;"></i>
+                </button>
+
+                <button type="button" class="btn btn-outline-danger btn-floating mx-1">
+                    <i class="fab fa-google" style="color: #DB4437;"></i>
+                </button>
+
+                <button type="button" class="btn btn-outline-info btn-floating mx-1">
+                    <i class="fab fa-twitter" style="color: #1DA1F2;"></i>
+                </button>
+
+                <button type="button" class="btn btn-outline-dark btn-floating mx-1">
+                    <i class="fab fa-github" style="color: #333;"></i>
+                </button>
+                
+            </div>
+</form>
+
+</div>
+ 
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -10,6 +10,9 @@
     
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
     <link rel="stylesheet" href="CSS/style.css" >
 
@@ -52,7 +55,7 @@
         <a class="nav-link me-2" href="#about">About</a>
       </li>
        <li class="nav-item">
-          <a class="nav-link me-2" href="#contact">Contact Us</a>
+          <a class="nav-link me-2" href="contact.php">Contact Us</a>
         </li>
 
 
